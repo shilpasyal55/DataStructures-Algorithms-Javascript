7503c70d195e4454bcafb5133731c07410b0d4b7
shilpa syal <shilpasyal55@gmail.com>
2019-08-30 22:01:01 +0530

    binary search using recusrion

--- binarysearch.js
+++ binarysearch.js
@@ -4,6 +4,7 @@
 //If middle element is the item to be searched then search is stopped there otherwise 
 //we again search the one of half of the array is searched
 
+//Using Iteration
 function BinarySearch(list,item) {
 
     let start = 0;
@@ -18,8 +19,30 @@ function BinarySearch(list,item) {
     return -1
 }
 
+let arr = [3, 5, 6, 10, 14, 25, 60];
+console.log(BinarySearch(arr, 6))
 
-let arr = [3,5,6,10,14,25,60]; 
-console.log(BinarySearch(arr,5))
+
+
+//Using Recursion
+
+function BinarySearchRecursion(list, start = 0, end = (list.length - 1), item) {
+
+        if( start > end ) return -1
+        let mid = Math.floor((start + end) / 2)
+        if (list[mid] == item) {
+            return mid;
+        }
+        else if (item > list[mid]) {
+            return BinarySearchRecursion(list, mid + 1, end, item)
+        }
+        else if (item < list[mid]) {
+           return BinarySearchRecursion(list, start, mid - 1, item)
+        }
+}
+
+
+
+console.log(BinarySearchRecursion(arr,0,(arr.length - 1), 6))
 
 
